8317f700862bb0d3765b84cf798c1b5696dd2a21
soyebZaynax <muktadir189@gmail.com>
2022-05-15 22:33:54 +0600

    First Commit Added BuildConfig File for JWT token and BaseUrl

---
+++ app/src/main/java/com/muktadir/currencyconverter/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.muktadir.currencyconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("ApiKey",BuildConfig.ApiKey)
+        Log.d("BaseUrl",BuildConfig.BaseUrl)
+
+    }
+}
